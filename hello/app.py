64884360f2f569c815564d27bd79158c29e9bc7b
woniuhuhu <jh18205234523@163.com>
2021-09-18 18:06:45 +0800

    增加环境变量文件

--- hello/app.py
+++ hello/app.py
@@ -1,13 +1,7 @@
-'''
-Author: your name
-Date: 2021-09-18 15:27:32
-LastEditTime: 2021-09-18 15:29:24
-LastEditors: Please set LastEditors
-Description: In User Settings Edit
-FilePath: \flask\hello\app.py
-'''
+
 from flask import Flask
 app = Flask(__name__)
-@app.rote('/')
-def index():
-    return '<h1>Hello world</h1>'
+@app.route('/')
+@app.route('/jh/<name>')
+def index(name='jh'):
+    return '<h1>Hello world,%s </h1>'%name 
